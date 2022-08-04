898b0935cbde69ce1b282af8689ac0974b25895e
Sebastian Badea <98695665+zoni123@users.noreply.github.com>
2022-08-04 11:45:17 +0300

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+x = input("Search: ")
+URL = "https://www.emag.ro/search/" + x
+page = requests.get(URL)
+content = BeautifulSoup(page.content, "html.parser")
+results = content.find(id="card_grid")
+info = results.find_all("div", class_="card-v2")
+print("\n---RESULTS---\n")
+for result in info:
+    name = result.find("a", class_="card-v2-title semibold mrg-btm-xxs js-product-url")
+    av = result.find("div", class_="card-estimate-placeholder")
+    price = result.find("p", class_="product-new-price")
+    article = result.find("a")["href"]
+    print(name.text)
+    print(av.text)
+    print(price.text)
+    print("ARTICOL: " + article)
+    print("\n")
