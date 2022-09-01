1525331b33cce8bb8c15c066a8733b69b2420e96
Sebastian Badea <98695665+zoni123@users.noreply.github.com>
2022-09-01 20:55:52 +0300

    Added spreadsheet functionality
    The script now creates the file output.xlsx and writes the retrieved data to it.

--- scraper.py
+++ scraper.py
@@ -1,7 +1,17 @@
 import requests
+from openpyxl import Workbook
+from openpyxl.styles import Font
 from bs4 import BeautifulSoup
 
 if __name__ == '__main__':
+    output = Workbook()
+    output_page = output.active
+    output_page.column_dimensions['A'].width = 5
+    output_page.column_dimensions['B'].width = 100
+    output_page.column_dimensions['C'].width = 25
+    output_page.column_dimensions['D'].width = 15
+    output_page.column_dimensions['E'].width = 10
+    output_page.column_dimensions['F'].width = 20
     i = 1
     x = input("Search: ")
     URL = "https://www.emag.ro/search/" + x
@@ -18,15 +28,26 @@ if __name__ == '__main__':
         review_n = result.find("span", class_="visible-xs-inline-block")
         article = result.find("a")["href"]
         print(str(i) + ". " + name.text)
+        output_page['A' + str(i)] = str(i)
+        output_page['A' + str(i)].font = Font(bold=True)
+        output_page['B' + str(i)] = name.text
         print(av.text)
+        output_page['C' + str(i)] = av.text
         print(price.text)
-        if(review != None):
-            if(review_n.text == "(1)"):
+        output_page['D' + str(i)] = price.text
+        if review is not None:
+            if review_n.text == "(1)":
                 print("Rating: " + review.text + "/5 -> (1) review")
+                output_page['E' + str(i)] = review.text + "/5"
+                output_page['F' + str(i)] = "(1) review"
             else:
                 print("Rating: " + review.text + "/5 -> " + review_n.text + " reviews")
+                output_page['E' + str(i)] = review.text + "/5"
+                output_page['F' + str(i)] = review_n.text + "reviews"
         else:
             print("No reviews yet")
+            output_page['E' + str(i)] = '-'
         print("ARTICLE: " + article)
         print("\n")
         i += 1
+    output.save("output.xlsx")
